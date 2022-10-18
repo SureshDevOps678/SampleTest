c6cddd582b9d50ac2bb8cc96bc7efa4a7d0dd5eb
SureshDevOps678 <rvnmails.r@gmail.com>
2022-10-18 22:15:04 +0530

    adding new file

---
+++ addition.java
@@ -0,0 +1,15 @@
+class addition
+{
+	public static void main(string ars[])
+	{
+		int num1,num2,res;
+		scanner s = new scanner(system.in);
+		num1=s.nextInt();
+		system.out.println("Enter num2 value");
+		num2=s.nextInt();
+		res=num1+num2;
+		system.out.println("The result is "+res);
+	
+	}
+}
+
